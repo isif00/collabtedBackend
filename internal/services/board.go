a65c2d70b73d5640c7e121f0b3e7a12a821ff34e
isif00 <sifodev@gmail.com>
2025-03-07 19:35:34 +0100

    [fix]: fix excalidraw

--- internal/services/board.go
+++ internal/services/board.go
@@ -23,10 +23,22 @@ func (s *BoardService) UpdateBoard(data types.BoardD, boardId string) (*db.Board
 		log.Fatalf("Error marshaling elements: %v", err)
 	}
 
+	jsonAppState, err := json.Marshal(data.AppState)
+	if err != nil {
+		log.Fatalf("Error marshaling appState: %v", err)
+	}
+
+	jsonFiles, err := json.Marshal(data.Files)
+	if err != nil {
+		log.Fatalf("Error marshaling files: %v", err)
+	}
+
 	updatedBoard, err := prisma.Client.Board.FindUnique(
 		db.Board.ID.Equals(boardId),
 	).Update(
 		db.Board.Elements.Set(jsonElements),
+		db.Board.AppState.Set(jsonAppState),
+		db.Board.Files.Set(jsonFiles),
 	).Exec(context.Background())
 
 	if err != nil {
@@ -42,9 +54,23 @@ func (s *BoardService) SaveBoard(data types.BoardD) (*db.BoardModel, error) {
 		log.Fatalf("Error marshaling elements: %v", err)
 	}
 
+	var appState, files *db.JSON
+
+	if data.AppState != nil {
+		tmpAppState := db.JSON(data.AppState)
+		appState = &tmpAppState
+	}
+
+	if data.Files != nil {
+		tmpFiles := db.JSON(data.Files)
+		files = &tmpFiles
+	}
+
 	result, err := prisma.Client.Board.CreateOne(
 		db.Board.WorkspaceID.Set(data.WorkspaceID),
 		db.Board.Elements.Set(prismaTypes.JSON(jsonElements)),
+		db.Board.AppState.SetIfPresent(appState),
+		db.Board.Files.SetIfPresent(files),
 	).Exec(context.Background())
 
 	if err != nil {
--- pkg/types/board.go
+++ pkg/types/board.go
@@ -6,5 +6,7 @@ import (
 
 type BoardD struct {
 	Elements    []json.RawMessage `json:"elements"`
+	AppState    json.RawMessage   `json:"appState"`
+	Files       json.RawMessage   `json:"files"`
 	WorkspaceID string            `json:"workspaceId"`
 }
